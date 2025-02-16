868c2ee9963ecd4588d5d75ec1b843c7f095a49a
mohil-sharma <mohil.sharma1@gmail.com>
2025-02-16 12:51:43 +0530

    cleanup

--- signin/vite.config.mjs
+++ signin/vite.config.mjs
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001, // Change this to your desired port
+    strictPort: true, // Prevents Vite from switching to a different port if 3001 is busy
+  },
   css: {
     postcss: 'postcss.config.mjs', // Correct the path (No need for `postcss:`)
   },
--- signup/vite.config.js
+++ signup/vite.config.js
@@ -4,6 +4,10 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Change this to your desired port
+    strictPort: true, // Prevents Vite from switching to a different port if 3001 is busy
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
